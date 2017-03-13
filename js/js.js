57ad6782ee019411ca421673fff2554bf6d3664c
zander mackenzie <amacke201@caledonian.ac.uk>
2017-03-13 18:06:12 +0000

    faq maps donate updates

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function showNav() {            //Drop down or pull up the mobile navigation whe
     var heightValue = $('#navigation').css('height');
         if (heightValue == '0px'){      //If nav up
             $( '#navigation' ).css({ "height": "290px", "-webkit-transition": "height 400ms ease-in", "transition": "height 400ms ease-in" });                //Drop down
+            $( '#pageHeader' ).css({ "max-height": "530px" });
         }
         else {          //If nav down
             $( '#navigation' ).css({ "height": "", "-webkit-transition": "height 400ms ease-out", "transition": "height 400ms ease-out" });    //Pull up
